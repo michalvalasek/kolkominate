68dccaf200686bc6d758c295bb836104925a2bf0
Michal Valasek <michal.valasek@gmail.com>
2011-12-08 16:09:29 +0100

    Initial commit - fresh project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kolkominate::Application.config.secret_token = 'ffccc24e4a4f287eb5dc3e15e0c1cfffd51746f77d07bf1927d6bf259893f9929408fbfbbf8c806ccc7b8072b1e148cb390e8aa933a8903be53ae28f83717246'
