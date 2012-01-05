742cdd7e88a9facd61a96f4221d0b4444fc9e34a
Michal Valasek <michal.valasek@gmail.com>
2012-01-05 17:40:13 +0100

    Refactored main menu - added special helper method for generating menu items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def main_menu_item_for(path, text, alt, img_name)
+    if current_page?(path)
+      link_to content_tag(:strong, image_tag("/images/nav/#{img_name}", alt: alt) + " #{text}"), path
+    else
+      link_to image_tag("/images/nav/#{img_name}", alt: alt) + " #{text}", path
+    end
+  end
+
 end
