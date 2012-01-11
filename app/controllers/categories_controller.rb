00dd62fd9afa1a38186888dac0a96857c88d6de2
Michal Valasek <michal.valasek@gmail.com>
2012-01-11 21:11:33 +0100

    categories controller & views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,23 @@
 class CategoriesController < ApplicationController
+
+  before_filter :authenticate_user!
+
+  # GET /categories
   def index
+    @categories = RocketTag::Tagging.select('*, COUNT(taggable_id) AS taggable_cnt').where("tagger_id=1 AND context='categories'").group('tag_id')
+
+    if params[:sort] == "expenses"
+      @categories = @categories.order('taggable_cnt DESC')
+    end
   end
 
+  # GET /category/1
   def show
+    @category = RocketTag::Tag.find(params[:id])
+    @taggings = @category.taggings.where("context='categories'")#.where("tagger_id=#{current_user.id}")
+
+    # chceme zobrazit zoznam vsetkych Expensov patriacich akt. userovi AND majucich aktualny tag ako kategoriu
+    @expenses = Expense.tagged_with([@category.name],on: "categories").where("user_id=#{current_user.id}").order("date DESC")
   end
 
 end
