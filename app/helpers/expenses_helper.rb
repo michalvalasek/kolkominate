542375d9b9c77d42a2c99d049e6c07a34834103d
Michal Valasek <michal.valasek@gmail.com>
2012-01-11 21:12:27 +0100

    categories_links helper method

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,10 @@
 module ExpensesHelper
+
+  def categories_links_for(expense)
+    expense.categories.map do |ctg_name|
+      ctg = RocketTag::Tag.find_by_name(ctg_name)
+      link_to ctg.name, category_path(ctg.id)
+    end.join(", ").html_safe
+  end
+
 end
