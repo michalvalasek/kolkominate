94d9330edcad8a21957c4e618104ae725e89b760
Michal Valasek <michal.valasek@gmail.com>
2012-01-27 02:18:16 +0100

    Changed quick-add-expense box to work with TokenInput jQ plugin via AJAX

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,10 +4,18 @@ class CategoriesController < ApplicationController
 
   # GET /categories
   def index
-    @categories = RocketTag::Tagging.select('*, COUNT(taggable_id) AS taggable_cnt').where("tagger_id=1 AND context='categories'").group('tag_id')
+    @categories = RocketTag::Tagging.select('*, COUNT(taggable_id) AS taggable_cnt').where("tagger_id=#{current_user.id} AND context='categories'").group('tag_id')
 
     if params[:sort] == "expenses"
       @categories = @categories.order('taggable_cnt DESC')
+    end
+
+    respond_to do |format|
+      format.html
+      format.json do
+        ctgs = ActiveRecord::Base.connection.execute("SELECT DISTINCT t.id, t.name FROM tags t RIGHT JOIN taggings ts ON ts.tag_id=t.id WHERE ts.tagger_id=#{current_user.id} AND t.name LIKE '#{params['q']}%' ORDER BY name")
+        render json: ctgs.map {|i| {id: i[1], name: i[1]} }
+      end
     end
   end
 
