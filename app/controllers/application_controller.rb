3a5b3fd47bd5d48e04a1de5fcebb2cddf533c9b3
Michal Valasek <michal.valasek@gmail.com>
2012-02-17 18:49:21 +0100

    Exception handling with custom error pages (404 and 500)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,24 @@ class ApplicationController < ActionController::Base
 	def after_sign_out_path_for(resource)
 		root_path
 	end
+
+  unless Rails.configuration.consider_all_requests_local
+    rescue_from Exception, :with => :render_error
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+    rescue_from ActionController::RoutingError, :with => :render_not_found
+    rescue_from ActionController::UnknownController, :with => :render_not_found
+    rescue_from ActionController::UnknownAction, :with => :render_not_found
+  end
+
+  private
+
+    def render_not_found(exception)
+      #log_error(exception)
+      render :template => "/error/404.html.erb", :status => 404
+    end
+
+    def render_error(exception)
+      #log_error(exception)
+      render :template => "/error/500.html.erb", :status => 500
+    end    
 end
